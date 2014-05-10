170fb7bb4e9e36e2d8afe1dcc1addd51c562ccc1
abdotalaat <abdotalaatfcih@gmail.com>
2014-05-10 14:24:29 +0200

    create project

---
+++ SplashActivity/src/com/example/gametutorial/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.gametutorial;
+
+import java.io.IOException;
+
+import org.andengine.engine.Engine;
+import org.andengine.engine.LimitedFPSEngine;
+import org.andengine.engine.camera.BoundCamera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.WakeLockOptions;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
+import android.view.KeyEvent;
+
+import com.example.manager.ResourcesManager;
+import com.example.manager.SceneManager;
+
+public class MainActivity extends BaseGameActivity 
+{
+
+	private BoundCamera camera;
+	private ResourcesManager resourcesManager;
+
+	@Override
+	public Engine onCreateEngine(EngineOptions pEngineOptions) 
+	{
+		// TODO Auto-generated method stub
+		return new LimitedFPSEngine(pEngineOptions, 60);
+	}
+
+	@Override
+	public EngineOptions onCreateEngineOptions() 
+	{
+		System.out.println("create Engine Option ***************************************************************");
+		camera = new BoundCamera(0, 0, 800, 530);
+		EngineOptions engineOptions = new EngineOptions(true,ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(800, 530), this.camera);
+		// set needs music and sound to true, so we will be able to use some
+		// audio for our game
+		engineOptions.getAudioOptions().setNeedsMusic(true).setNeedsSound(true);
+		engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+		return engineOptions;
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) 
+	{
+		System.out.println("Key Down ***************************************************************");
+		if (keyCode == KeyEvent.KEYCODE_BACK) 
+		{
+			SceneManager.getInstance().getCurrentScene().onBackKeyPressed();
+		}
+		return false;
+	}
+
+	@Override
+	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback)throws IOException 
+	{
+		System.out.println("create Resources ***************************************************************");
+		ResourcesManager.prepareManager(mEngine, this, camera,getVertexBufferObjectManager());
+		resourcesManager = ResourcesManager.getInstance();
+		pOnCreateResourcesCallback.onCreateResourcesFinished();
+
+	}
+
+	
+	//Creating splashScreen
+	@Override
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)throws IOException 
+	{
+		SceneManager.getInstance().createMenuScene(pOnCreateSceneCallback);
+	}
+
+	@Override
+	public void onPopulateScene(Scene pScene,OnPopulateSceneCallback pOnPopulateSceneCallback)throws IOException 
+	{
+		
+		System.out.println("Load Menu After Splash Screen********************");
+		/*
+		mEngine.registerUpdateHandler(new TimerHandler(2f,new ITimerCallback() 
+		{
+					public void onTimePassed(final TimerHandler pTimerHandler)
+					{
+						mEngine.unregisterUpdateHandler(pTimerHandler);
+						//SceneManager.getInstance().createMenuScene();
+					}
+		}));
+		*/
+		pOnPopulateSceneCallback.onPopulateSceneFinished();
+
+	}
+
+}
---
+++ SplashActivity/src/com/example/scene/LevelsMenuScene.java
@@ -0,0 +1,116 @@
+package com.example.scene;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.andengine.entity.scene.menu.item.IMenuItem;
+import org.andengine.entity.scene.menu.item.SpriteMenuItem;
+import org.andengine.entity.scene.menu.item.decorator.ScaleMenuItemDecorator;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.util.GLState;
+
+import android.widget.Toast;
+
+import com.example.base.BaseScene;
+import com.example.manager.ResourcesManager;
+import com.example.manager.SceneManager;
+import com.example.manager.SceneManager.SceneType;
+
+public class LevelsMenuScene extends BaseScene implements IOnMenuItemClickListener{
+
+	
+	///////////////////////////////
+	//variables
+	private MenuScene submenuChildScene;
+	private final int MENU_OPTIONS = 0;
+	private final int MENU_QUIT = 1;
+	private final float     WIDTH  = 400;
+	 private final float     HEIGHT  = 240;
+	////////////////////
+	public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
+			float pMenuItemLocalX, float pMenuItemLocalY) {
+		// TODO Auto-generated method stub
+		switch(pMenuItem.getID())
+		{
+			case MENU_OPTIONS:
+//				ResourcesManager.getInstance().activity.runOnUiThread(new Runnable() {
+//			        public void run() {
+//			         
+//				//Toast.makeText(ResourcesManager.getInstance().activity, "You choose Level 1", Toast.LENGTH_SHORT).show();
+//			      
+//			        }
+//				});
+				SceneManager.getInstance().loadGameScene(engine);
+			   return true;
+		 default:
+		return false;
+		}
+	}
+
+	@Override
+	public void createScene() {
+		createBackground();
+		createMenuChildScene();
+		
+	}
+
+	@Override
+	public void onBackKeyPressed() {
+		SceneManager.getInstance().getMenuSceneBack(engine);
+	}
+
+	@Override
+	public SceneType getSceneType() {
+		return SceneType.SCENE_SUBMENU;
+	}
+
+	@Override
+	public void disposeScene() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	///////////////////////////////////////////////////////////
+	private void createBackground()
+	{
+		attachChild(new Sprite(WIDTH,HEIGHT, resourcesManager.submenu_background_region, vbom)
+		{
+    		@Override
+            protected void preDraw(GLState pGLState, Camera pCamera) 
+    		{
+                super.preDraw(pGLState, pCamera);
+                pGLState.enableDither();
+            }
+		});
+	}
+	
+	private void createMenuChildScene()
+	{
+		submenuChildScene = new MenuScene(camera);
+		submenuChildScene.setPosition(WIDTH,HEIGHT);
+		
+		final IMenuItem optionsMenuItem = new ScaleMenuItemDecorator(new SpriteMenuItem(MENU_OPTIONS, resourcesManager.submenu_item1_region, vbom), 1.2f, 1);
+		//final IMenuItem quitMenuItem = new ScaleMenuItemDecorator(new SpriteMenuItem(MENU_QUIT,resourcesManager.quit_region,vbom),1.2f,1);
+		
+		submenuChildScene.addMenuItem(optionsMenuItem);
+		
+		
+		submenuChildScene.buildAnimations();  //must setposition after it unless it has no effect
+		submenuChildScene.setBackgroundEnabled(false);
+		
+		
+//		playMenuItem.setPosition(playMenuItem.getX()-20, playMenuItem.getY() - 150);
+//		optionsMenuItem.setPosition(optionsMenuItem.getX()-20, optionsMenuItem.getY() - 110);
+		
+		optionsMenuItem.setPosition((WIDTH/2 - optionsMenuItem.getWidth()/2)-50,(HEIGHT/2 - optionsMenuItem.getHeight()/2)-50);
+		
+		
+		submenuChildScene.setOnMenuItemClickListener(this);
+		
+		setChildScene(submenuChildScene);
+		
+	}
+
+}
